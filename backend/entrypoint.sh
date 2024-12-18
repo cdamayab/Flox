dbea3b99f78fbb8a395ebb58b87978b655858e33
cdamayab <cdamayab@gmail.com>
2024-12-18 15:30:50 -0500

    flox backend: Docker-compose fixed and readme completed - Updated the README to include a new section on Excel Report Service and usage examples. - Modified the entrypoint script to ensure the application compiles and runs automatically when executing `docker-compose up`. - Adjusted Docker Compose configuration:   - Renamed containers for clarity.   - Removed volume `mysql-data:/var/lib/mysql` to prevent version incompatibilities   - Data is not persistent now.   - Added port 5005 for debugging purposes.

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,29 +3,16 @@
 # Path to compiled jar
 JAR_FILE=/app/build/libs/Flox-0.0.1-SNAPSHOT.jar 
 
-if [ "$1" == "build" ]; then
-    echo "Compilando la aplicación..."
-    ./gradlew build --no-daemon
-    echo "Compilación completada."
-    exit 0
-elif [ "$1" == "serve" ]; then
-    if [ -f "$JAR_FILE" ]; then
-        echo "Archivo $JAR_FILE encontrado. Iniciando aplicación..."
-        java -jar "$JAR_FILE"
-    else
-        echo "Archivo $JAR_FILE no encontrado. Por favor compile la aplicación usando 'build'."
-        exit 1
-    fi
+# Without args
+if [ "$RUN_MODE" == "dev" ]; then
+    echo "Iniciando en modo desarrollo con bootRun..."
+    ./gradlew bootRun --continuous -Dspring-boot.run.jvmArguments="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005"
+elif [ -f "$JAR_FILE" ]; then
+    echo "Archivo $JAR_FILE encontrado. Iniciando aplicación..."
+    java -jar "$JAR_FILE"
 else
-    # Without args
-    if [ "$RUN_MODE" == "dev" ]; then
-        echo "Iniciando en modo desarrollo con bootRun..."
-        ./gradlew bootRun --no-daemon
-    elif [ -f "$JAR_FILE" ]; then
-        echo "Archivo $JAR_FILE encontrado. Iniciando aplicación..."
-        java -jar "$JAR_FILE"
-    else
-        echo "Archivo $JAR_FILE no encontrado. Contenedor creado, listo para construir la aplicación."
-        tail -f /dev/null
-    fi
+    echo "Archivo $JAR_FILE no encontrado."
+    echo "Iniciando en modo producción, compilando JAR..."
+    ./gradlew clean build --no-daemon
+    java -jar "$JAR_FILE"
 fi
